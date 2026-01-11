0ea8cc5939a6afabe827be51f0f4bd997697c1e9
Nattaphat Songsi-in <eakacit@gmail.com>
2026-01-11 16:51:13 +0000

    composed and ready to use the pipeline  and database and pgadmin

--- poggeg/ingest_data.py
+++ poggeg/ingest_data.py
@@ -3,8 +3,11 @@
 
 # In[18]:
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+
+
 # In[19]:
 dtype = {
     "VendorID": "Int64",
@@ -39,21 +42,32 @@ parse_dates = [
 
 # In[21]:
 
-def run():
-    year = 2021
-    month = 1
-
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-    chunksize=100000
-    target_table = 'yellow_taxi_data'
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
+    # year = 2021
+    # month = 1
+# 
+#     pg_user = 'root'
+#     pg_pass = 'root'
+#     pg_host = 'localhost'
+#     pg_port = '5432'
+#     pg_db = 'ny_taxi'
+    # chunksize=100000
+    # target_table = 'yellow_taxi_data'
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-    engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+
 
     df_iter = pd.read_csv(
         url,
@@ -75,7 +89,10 @@ def run():
             df_chunk.head(0).to_sql(
                 name=target_table, 
                 con=engine, 
-                if_exists='append')
+                # what if data already exist 
+                # replace = drop and reinsert
+                # append = append
+                if_exists='replace')
             first = False
         df_chunk.to_sql(
             name=target_table, 
